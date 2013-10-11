e817d6603095c28d08f934c3dc99a8b15a9b5940
Rudolf Olah <rolah@outsideiq.com>
2013-10-11 14:41:51 -0400

    added keywords for angular-html-mode

--- angular-html-mode.el
+++ angular-html-mode.el
@@ -16,10 +16,72 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+(defvar angular-html-font-lock-keywords
+  (list
+   (regexp-opt '("ng-app"
+                 "ng-bind"
+                 "ng-bind-html"
+                 "ng-bind-template"
+                 "ng-blur"
+                 "ng-change"
+                 "ng-checked"
+                 "ng-class"
+                 "ng-class-even"
+                 "ng-class-odd"
+                 "ng-click"
+                 "ng-cloak"
+                 "ng-controller"
+                 "ng-copy"
+                 "ng-csp"
+                 "ng-cut"
+                 "ng-dblclick"
+                 "ng-disabled"
+                 "ng-focus"
+                 "ng-form"
+                 "ng-hide"
+                 "ng-href"
+                 "ng-if"
+                 "ng-include"
+                 "ng-init"
+                 "ng-keydown"
+                 "ng-keypress"
+                 "ng-keyup"
+                 "ng-list"
+                 "ng-model"
+                 "ng-mousedown"
+                 "ng-mouseenter"
+                 "ng-mouseleave"
+                 "ng-mousemove"
+                 "ng-mouseover"
+                 "ng-mouseup"
+                 "ng-non-bindable"
+                 "ng-open"
+                 "ng-paste"
+                 "ng-pluralize"
+                 "ng-readonly"
+                 "ng-repeat"
+                 "ng-selected"
+                 "ng-show"
+                 "ng-src"
+                 "ng-srcset"
+                 "ng-style"
+                 "ng-submit"
+                 "ng-switch"
+                 "ng-transclude"
+                 "ng-value"))
+   "{{.+?}}" ; angular expressions
+))
+
 (define-derived-mode angular-html-mode
   html-mode
   "HTML[Angular]"
-  "Major mode for AngularJS.
-\\{javascript-mode-map}")
+  "Major HTML mode for AngularJS.
+\\{html-mode-map}"
 
-(defconst angular-font-lock-keywords-1
+  (setq font-lock-defaults (list
+                            (append sgml-font-lock-keywords
+                                    sgml-font-lock-keywords-1
+                                    sgml-font-lock-keywords-2
+                                    angular-html-font-lock-keywords))))
+
+(provide 'angular-html-mode)
