c9ec7719eeb90e0404f8f60d7bb1ab712082d97e
Ryan Prior <ryanprior@gmail.com>
2015-12-01 13:37:18 -0600

    Adds metadata for angular-html-mode

--- angular-html-mode.el
+++ angular-html-mode.el
@@ -1,22 +1,34 @@
-;; angular-html-mode.el --- Major mode for Angular.js HTML file
+;; angular-html-mode.el --- Major mode for Angular.js HTML files
+
+;; Copyright (C) 2013-2015 Rudolf Olah
+
+;; Author: Rudolf Olah <omouse@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;; URL: https://github.com/omouse/angularjs-mode
+;; Keywords: languages javascript html
+
+;;; Commentary:
 
 ;; Angular HTML Mode is based on HTML mode. It adds keyword
 ;; highlighting for directives that are part of core AngularJS.
 
-;; Copyright (C) 2013 Rudolf Olah <omouse@gmail.com>
+;;; License:
 
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;; along with this file. If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
 ;;; Code:
 
 (defvar angular-html-font-lock-keywords
